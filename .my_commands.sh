f7f071bd3179e743212d831bf0189a5af4388a2b
Diogo Mota Pinto <diogomotapinto@gmail.com>
2019-06-01 14:31:56 +0100

    Changes

--- .my_commands.sh
+++ .my_commands.sh
@@ -4,9 +4,9 @@
 function create() {
     cd
     python create.py $1
-    cd /Users/kalle/Documents/Projects/MyProjects/$1
+    cd /Users/diogomotapinto/Projects/$1
     git init
-    git remote add origin git@github.com:KalleHallden/$1.git
+    git remote add origin git@github.com:diogomotapinto/$1.git
     touch README.md
     git add .
     git commit -m "Initial commit"
--- create.py
+++ create.py
@@ -1,30 +1,39 @@
 import sys
-import os 
+import os
 from selenium import webdriver
+import constants
 
-
-path = "/Users/kalle/Documents/Projects/MyProjects/"
+path = "/Users/diogomotapinto/Projects/"
 browser = webdriver.Chrome()
-browser.get('http://github.com/login')
+browser.get("http://github.com/login")
 
-username = ''
-password = ''
 
 def create():
     folderName = str(sys.argv[1])
     os.makedirs(path + str(sys.argv[1]))
     python_button = browser.find_elements_by_xpath("//input[@name='login']")[0]
-    python_button.send_keys(username) 
-    python_button = browser.find_elements_by_xpath("//input[@name='password']")[0] 
-    python_button.send_keys(password)
+    python_button.send_keys(constants.USERNAME)
+    python_button = browser.find_elements_by_xpath("//input[@name='password']")[0]
+    python_button.send_keys(constants.PASSWORD)
     python_button = browser.find_elements_by_xpath("//input[@name='commit']")[0]
     python_button.click()
-    browser.get('https://github.com/new') 
-    python_button = browser.find_elements_by_xpath("//input[@name='repository[name]']")[0] 
+    browser.get("https://github.com/new")
+    python_button = browser.find_elements_by_xpath("//input[@name='repository[name]']")[
+        0
+    ]
     python_button.send_keys(folderName)
-    python_button = browser.find_element_by_css_selector('button.first-in-line')
+    python_button.click()
+    python_button = browser.find_element_by_xpath(
+        "//input[@id='repository_visibility_private']"
+    )
+    python_button.click()
+    python_button = browser.find_element_by_css_selector("button.first-in-line")
     python_button.submit()
     browser.quit()
 
+
 if __name__ == "__main__":
     create()
+
+# <input class="mt-2 js-privacy-toggle" aria-describedby="private-description" type="radio" value="private" name="repository[visibility]" id="repository_visibility_private">
+
